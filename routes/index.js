88c069e32368b9f99e769ac75e5767f299363651
MMGInstructor <instructor@msi.example.com>
2025-11-09 09:52:00 +0100

    cambio varios 2

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db/config')
+const pool = require('../db/config')
 
 /* Show home page. */
 router.get('/', function(req, res) {
   // we first check if the 'contacts' table exists
-  db.pool.query("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'contacts')", function(err,results) {
+   pool.query("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'contacts')", function(err,results) {
     if (err) {
       console.log(err);
       res.render('index', { error: 'Database connection failure! '+err.stack, contacts: null, title: 'Contact List' });
@@ -18,7 +18,7 @@ router.get('/', function(req, res) {
 
     // 'contacts' table exists. Show the records.
     else {
-      db.pool.query('SELECT * FROM contacts', function(err,results) {
+      pool.query('SELECT * FROM contacts', function(err,results) {
         if (err) {
           console.log(err);
           res.render('index', { error: 'Database connection failure! '+err.stack, contacts: null, title: 'Contact List' });
@@ -36,7 +36,7 @@ router.get('/', function(req, res) {
 /* Seed test data */
 router.post('/seed', function(req,res) {
   // drop 'contacts' table if already exists, and seed some test data
-  db.pool.query("drop table if exists contacts; create table contacts(id serial primary key,firstname varchar(30) not null,lastname varchar(30) not null, email varchar(30) not null); insert into contacts(firstname, lastname, email) values ('Bilbo','Baggins','bilbo@theshire.com'),('Frodo','Baggins','frodo@theshire.com'),('Samwise','Gamgee','sam@theshire.com'),('Peregrin','Took','pippin@theshire.com'),('Meriadoc','Brandybuck','merry@theshire.com')",function(err,results) {
+  pool.query("drop table if exists contacts; create table contacts(id serial primary key,firstname varchar(30) not null,lastname varchar(30) not null, email varchar(30) not null); insert into contacts(firstname, lastname, email) values ('Bilbo','Baggins','bilbo@theshire.com'),('Frodo','Baggins','frodo@theshire.com'),('Samwise','Gamgee','sam@theshire.com'),('Peregrin','Took','pippin@theshire.com'),('Meriadoc','Brandybuck','merry@theshire.com')",function(err,results) {
     if (err) {
       console.log(err);
       res.render('index', { error: 'Seeding database failure! '+err.stack, contacts: null, title: 'Contact List' });
