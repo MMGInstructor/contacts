8182175ad0e9098deeaba6f8e1f6528713470a45
MMGInstructor <instructor@msi.example.com>
2025-11-09 09:56:37 +0100

    codigo mysql simple

--- db/config.js
+++ db/config.js
@@ -5,22 +5,15 @@ const mysql = require('mysql2/promise');
 // DB_CONFIG = mysql://usuario:password@localhost:3306/nombre_basedatos
 const DB_CONFIG = process.env.DB_CONFIG || 'mysql://contacts:contacts@localhost:3306/contacts';
 
-// Crear el pool de conexiones
-const pool = mysql.createPool({
-  uri: DB_CONFIG,
-  waitForConnections: true,
-  connectionLimit: 10, // número máximo de conexiones simultáneas
-  queueLimit: 0        // 0 = sin límite de solicitudes en cola
-});
-
-pool.getConnection()
-  .then(conn => {
+async function createConnection() {
+  try {
+    const connection = await mysql.createConnection(DB_CONFIG);
     console.log('✅ Conexión a MySQL establecida correctamente');
-    conn.release();
-  })
-  .catch(err => {
-    console.error('❌ Error al conectar con MySQL:', err);
-  });
+    return connection;
+  } catch (error) {
+    console.error('❌ Error al conectar con MySQL:', error);
+    throw error;
+  }
+}
 
-// Exportar el pool para usarlo en otros módulos
-module.exports = pool;
+module.exports = { createConnection };
